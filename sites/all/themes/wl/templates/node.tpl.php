80cab69d1f3460ca24a1f7395e6277168984c1d6
Lucian Dipeso <ldipeso@gmail.com>
2013-07-12 15:50:25 -0700

    Various bugfixes.

--- sites/all/themes/wl/templates/node.tpl.php
+++ sites/all/themes/wl/templates/node.tpl.php
@@ -1,23 +1,23 @@
 <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-  <?php if($wl_show_title): ?>
-    <header>
-      <?php print render($title_prefix); ?>
-      <?php if (!$page && $title): ?>
-        <h2<?php print $title_attributes; ?>>
-          <?php print $title; ?><?php if($wl_add_colon_to_title): print ":"; endif; ?>
-          <?php print $wl_subtitle ?>
-        </h2>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
+  <header>
+    <?php print render($title_prefix); ?>
 
-      <?php if ($display_submitted): ?>
-        <span class="submitted">
-          <?php print $user_picture; ?>
-          <?php print $submitted; ?>
-        </span>
-      <?php endif; ?>
-    </header>
-  <?php endif; ?>
+    <?php if (!$page && $title && $wl_show_title): ?>
+      <h2<?php print $title_attributes; ?>>
+        <?php print $title; ?><?php if($wl_add_colon_to_title): print ":"; endif; ?>
+        <?php print $wl_subtitle ?>
+      </h2>
+    <?php endif; ?>
+    
+    <?php print render($title_suffix); ?>
+
+    <?php if ($display_submitted): ?>
+      <span class="submitted">
+        <?php print $user_picture; ?>
+        <?php print $submitted; ?>
+      </span>
+    <?php endif; ?>
+  </header>
 
   <?php
     // Hide comments, tags, and links now so that we can render them later.
